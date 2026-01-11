2656f1d5e01d1799f49581aed6d535b00a2103ee
Bluore <2260762800@qq.com>
2026-01-11 15:46:40 +0800

    添加：cors中间件

--- server/internal/api/router/router.go
+++ server/internal/api/router/router.go
@@ -9,19 +9,20 @@ import (
 )
 
 func Setup(r *gin.Engine, h *handler.Handler, cfg *config.Config) {
-	// todo 全局中间件
+	// 全局中间件
+	r.Use(middleware.CORS())
 
 	// 设置路由
 	v1 := r.Group("/api/v1")
 	{
-		record := v1.Group("/record")
+		record := v1.Group("/admin")
 		record.Use(middleware.AuthToken(cfg))
 		{
-			record.POST("", h.UploadRecord)
+			record.POST("/record", h.UploadRecord)
 		}
-		recordGuest := v1.Group("/record/guest")
+		recordGuest := v1.Group("/guest")
 		{
-			recordGuest.GET("", h.GetLastRecord)
+			recordGuest.GET("/record", h.GetLastRecord)
 		}
 	}
 }
