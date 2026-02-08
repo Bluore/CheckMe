deb55aa708ca3dbd628f23c8a1974d263c338dfa
Bluore <2260762800@qq.com>
2026-02-08 23:47:02 +0800

    添加戳一戳和稍一句话类型的消息

--- pkg/request/feishu_request.go
+++ pkg/request/feishu_request.go
@@ -193,6 +193,116 @@ func NotifyDataOfEncourage(cfg *config.Config, c *gin.Context, msg string) (data
 	return resp, nil
 }
 
+func NotifyDataOfDropLine(cfg *config.Config, c *gin.Context, msg string) (datatypes.JSON, error) {
+	body := `
+{
+  "msg_type": "interactive",
+  "card": {
+    "header": {
+      "title": {
+        "tag": "plain_text",
+        "content": "%s"
+      }
+    },
+    "elements": [
+      {
+        "tag": "div",
+        "text": {
+          "tag": "lark_md",
+          "content": "稍一句话~"
+        }
+      },
+      {
+        "tag": "div",
+		"collapsible": true,
+        "text": {
+          "tag": "lark_md",
+          "content": "**Sender IP**: *%s* \n**Sender UA**: *%s*"
+        }
+      },
+      {
+        "tag": "action",
+        "actions": [
+          {
+            "tag": "button",
+            "text": {
+              "tag": "plain_text",
+              "content": "查看详情"
+            },
+            "type": "primary",
+            "url": "https://me.bluore.top"
+          }
+        ]
+      }
+    ]
+  }
+}
+`
+	body = fmt.Sprintf(body, msg, c.ClientIP(), c.GetHeader("User-Agent"))
+
+	resp, err := NotifyOnFeishu(cfg.Notify.FeishuBot, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func NotifyDataOfTapTap(cfg *config.Config, c *gin.Context, msg string) (datatypes.JSON, error) {
+	body := `
+{
+  "msg_type": "interactive",
+  "card": {
+    "header": {
+      "title": {
+        "tag": "plain_text",
+        "content": "戳了戳你~"
+      }
+    },
+    "elements": [
+      {
+        "tag": "div",
+        "text": {
+          "tag": "lark_md",
+          "content": "%s"
+        }
+      },
+      {
+        "tag": "div",
+		"collapsible": true,
+        "text": {
+          "tag": "lark_md",
+          "content": "**Sender IP**: *%s* \n**Sender UA**: *%s*"
+        }
+      },
+      {
+        "tag": "action",
+        "actions": [
+          {
+            "tag": "button",
+            "text": {
+              "tag": "plain_text",
+              "content": "查看详情"
+            },
+            "type": "primary",
+            "url": "https://me.bluore.top"
+          }
+        ]
+      }
+    ]
+  }
+}
+`
+	body = fmt.Sprintf(body, msg, c.ClientIP(), c.GetHeader("User-Agent"))
+
+	resp, err := NotifyOnFeishu(cfg.Notify.FeishuBot, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
 func NotifyFeishu(cfg *config.Config, c *gin.Context, data dto.CreateNotifyRequest) error {
 	switch data.Type {
 	case "study":
@@ -205,6 +315,14 @@ func NotifyFeishu(cfg *config.Config, c *gin.Context, data dto.CreateNotifyReque
 		}
 	case "encourage":
 		if _, err := NotifyDataOfEncourage(cfg, c, data.Msg); err != nil {
+			return err
+		}
+	case "DropLine":
+		if _, err := NotifyDataOfDropLine(cfg, c, data.Msg); err != nil {
+			return err
+		}
+	case "TapTap":
+		if _, err := NotifyDataOfTapTap(cfg, c, data.Msg); err != nil {
 			return err
 		}
 	default:
